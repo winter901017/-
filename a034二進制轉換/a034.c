a0a190b0639179027898c2959bee4dfb0a0aaa68
28095116 <a28095116@gmail.com>
2020-12-16 14:49:40 +0800

    update

--- a034二進制轉換/a034.c
+++ a034二進制轉換/a034.c
@@ -3,14 +3,23 @@
 int main(){
   int input=0;
   while(scanf("%d",&input)!=EOF){
+    int covert[99];
+    int countbit=0;
     do{
         if(input%2==0){
-          printf("0");
+          covert[countbit]=0;
         }else{
-          printf("1");
+          covert[countbit]=1;
         };
         input/=2;
+        countbit++;
     }while(input!=0);
+    for(int i=countbit-1;i>=0;i--){
+      printf("%d",covert[i]);
+    };
+    for(int i=countbit-1;i>=0;i--){
+    };
+      printf("\n");
   };
   return 0;
 };
