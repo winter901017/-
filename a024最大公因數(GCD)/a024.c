49c725e4af0c8fcd1f8832a58be9f60577742887
28095116 <a28095116@gmail.com>
2020-12-17 09:53:11 +0800

    update

--- a024最大公因數(GCD)/a024.c
+++ a024最大公因數(GCD)/a024.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 int main(){
   int a=0,b=0;
-  while(scanf("%d%d",a,b)!=EOF){
-
+  int value=0;
+  while(scanf("%d%d",&a,&b)!=EOF){
+    do{
+      if(a<b){
+        int tmp;
+        tmp=a;
+        a=b;
+        b=tmp;
+      };
+      if(a%b==0){
+        value=b;
+      };
+      a=a%b;
+    }while((a!=0)&&(b!=0));
+    printf("%d\n",value);
   };
   return 0;
 };
