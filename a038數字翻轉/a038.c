aa5be3e8f6e7d1c4912728db259c8d3a255f72a1
28095116 <a28095116@gmail.com>
2020-12-17 19:58:55 +0800

    updat

--- a038數字翻轉/a038.c
+++ a038數字翻轉/a038.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
-
-int mian(){
+int main(){
 
   int num=0;
-  int count=0;
   while(scanf("%d",&num)!=EOF){
+      int count=0;
+      int zero=0;
+      int jump=0;
+      if(num==0){
+        printf("0\n");
+        jump=1;
+      };
     do{
+      if(jump==1)break;
+      if(num%10!=0)zero=1;
+      if((num!=0)&&(zero!=0)){
+        printf("%d",num%10);
+      };
       num/=10;
+      if((num==0)&&(count==1))printf("0");
+      if(num==0)printf("\n");
       count++;
     }while(num!=0);
-    printf("%d",count);
+    //printf("%d",count);
   };
 
   return 0;
