6fd12bb85883547815f6629a07415c3b11085412
winter901017 <a28095116@gmail.com>
2020-12-30 00:19:16 +0800

    update

--- a224_明明愛明明/a224.c
+++ a224_明明愛明明/a224.c
@@ -1,28 +1,30 @@
 #include <stdio.h>
 #include <string.h>
 int main (){
-  int  str[1000]={0};
-  char n;
-  int count=0;
-  int flag=0;
-
-
-    if((n>='a')&&(n<='z')){
-        str[count]=n-'a';
+  char str_str[1000]={0};
+  while(gets(str_str)!=NULL){
+    int  str[1000]={0};
+    int count=0,str_count=0;
+    int flag=0;
+    str_count=strlen(str_str);
+    for(int i=0;i<str_count;i++){
+      if((str_str[i]>='a')&&(str_str[i]<='z')){
+          str[count]=str_str[i]-'a';
+          count++;
+      }else if((str_str[i]>='A')&&(str_str[i]<='Z')){
+        str[count]=str_str[i]-'A';
         count++;
-    }else if((n>='A')&&(n<='Z')){
-      str[count]=n-'A';
-      count++;
+      };
     };
-
-  for(int i=0;i<count;i++){
-    int j=count-1-i;
-    if(str[i]!=str[j])flag=1;
-  };
-  if(flag==1){
-    printf("no...");
-  }else{
-    printf("yes !");
-  };
+    for(int i=0;i<count;i++){
+      int j=count-1-i;
+      if(str[i]!=str[j])flag=1;
+    };
+    if(flag==1){
+      printf("no...\n");
+    }else{
+      printf("yes !\n");
+    };
+  }
   return 0;
 }
